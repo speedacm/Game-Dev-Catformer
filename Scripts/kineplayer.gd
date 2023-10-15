469a33df9b2dcec75d006e46359d62a1ae9b8e4a
wDvy <69913344+wDvy@users.noreply.github.com>
2023-10-15 14:17:04 -0400

    added statics

--- Scripts/kineplayer.gd
+++ Scripts/kineplayer.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
-var friction = 0.5
+var friction = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,14 +13,11 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
 	var movement = move_and_slide()
 	
 	if get_last_slide_collision() != null:
 		friction = get_last_slide_collision().get_collider().get_friction()
 		print(friction)
-	
-
 	
 	if is_on_floor():
 		var direction = Input.get_axis("ui_left", "ui_right")
@@ -28,7 +25,6 @@ func _physics_process(delta):
 			velocity.x = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0 , SPEED*friction)
-			
 	
 
 
