20161b64f1b3681ed63f10b08c6eebd26a5d2178
Devin Ward <69913344+wDvy@users.noreply.github.com>
2023-10-20 18:01:51 -0400

    Dw new statics (#17)
    * Dw add static bodies (#16)
    
    * add statics
    
    * added static test asset tilemap
    
    * fixed ui coll
    
    * add get_friction func
    
    * friction added
    
    The cat object can now call the friction of the object it is currently on
    
    * Revert "Merge branch 'master' into dw-add-static-bodies"
    
    This reverts commit 895f9cb18c5c9fd32ae6a072ebf6146b4047a724, reversing
    changes made to d5ce47e72ca5fa2befb6a1ae985dba9e519cb8e1.
    
    ---------
    
    Co-authored-by: Bluedog77 <dgcooley04@gmail.com>
    
    * added statics
    
    * camera change
    
    ---------
    
    Co-authored-by: Bluedog77 <dgcooley04@gmail.com>

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -17,7 +17,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
 	if (Input.is_action_pressed("Click")):
 		drag = true
 	elif (Input.is_action_just_released("Click")): 
