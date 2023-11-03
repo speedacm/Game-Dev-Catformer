1d2dc03991e355c6d6ed7849c0f4bc55edf7de01
Luke Werner <129887980+LukeWerner@users.noreply.github.com>
2023-11-03 19:25:07 -0400

    this sucks

---
+++ scratchpad.gd
@@ -0,0 +1,16 @@
+extends CollisionShape2D
+
+@export var friction  = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_scratch_pad_area_entered(area):
+	pass # Replace with function body.
