09e4d047c84353b54b4e1f8612cccc26e4cfcee3
Devin Ward <69913344+wDvy@users.noreply.github.com>
2023-12-08 18:52:40 -0500

    Added sleep/wake animation (#43)
    Co-authored-by: BigHBen <hdbenson17@gmail.com>

--- Scripts/kineplayer.gd
+++ Scripts/kineplayer.gd
@@ -18,17 +18,22 @@ var Holding = false
 var canwalljump = false
 var exiting = false
 var smallHopping = false
+var awake = false
+var callonce = 0
+var sprinting = false
 
 signal falling()
 
 func _ready():
 	connect('falling', dragline._on_falling)
 
-
 #Executes Every Frame
 func _physics_process(delta):
 	
-	if sleeping == false:
+	IsSleep()
+	
+	if sleeping == false && awake == true:
+	
 		var movement = move_and_slide()
 		
 		Fall(delta)
@@ -47,8 +52,6 @@ func _physics_process(delta):
 
 			
 
-
-
 #Function Defintions
 	
 	
@@ -59,7 +62,8 @@ func AnimatePlayer():
 	
 	if is_on_floor():
 		#Play walk animation if moving on floor
-		if (velocity.x!=0): $AnimationPlayer.play("Walk")
+		if (velocity.x!=0 && !sprinting): $AnimationPlayer.play("Walk")
+		elif (velocity.x!=0 && sprinting): $AnimationPlayer.play("Run")
 		#Play charge animation if dragging mouse
 		elif (Input.is_action_just_pressed("Click")):
 			mouse_start = get_global_mouse_position()
@@ -121,7 +125,10 @@ func UserMovement(delta):
 			direction += 1
 		var sprint = 1
 		if Input.is_action_pressed("sprint"):
+			sprinting = true
 			sprint = 1.5
+		if Input.is_action_just_released("sprint"):
+			sprinting = false
 		if direction:
 			direction /= abs(direction)
 			velocity.x = direction * SPEED * sprint
@@ -154,3 +161,13 @@ func _on_wallexit():
 	canwalljump = false
 	
 	
+func IsSleep():
+	if sleeping == false && awake == false && callonce == 0:
+		callonce = 1
+		$AnimationPlayer.play("Wake") 
+
+func wakeup_anim_finished(anim_name):
+	if anim_name == "Wake":
+		print("Wakey wakey")
+		awake = true
+	pass # Replace with function body.
